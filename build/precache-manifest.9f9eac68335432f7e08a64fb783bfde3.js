8e60a33735eb68c9ba81e70bc95dedb3d2a8740e
Prasad Patil <prasadrpatil25.10@gmail.com>
2020-10-09 12:32:33 +0530

    first commit

---
+++ build/precache-manifest.9f9eac68335432f7e08a64fb783bfde3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07afcff1cba714dac55f",
+    "url": "/static/css/main.f1ec07b2.chunk.css"
+  },
+  {
+    "revision": "07afcff1cba714dac55f",
+    "url": "/static/js/main.aa3e6bb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b85b574e234650f49b6",
+    "url": "/static/css/2.323e3b04.chunk.css"
+  },
+  {
+    "revision": "3b85b574e234650f49b6",
+    "url": "/static/js/2.5453e4f8.chunk.js"
+  },
+  {
+    "revision": "0d49ae7070a753a5267c5b8f4be39c00",
+    "url": "/index.html"
+  }
+];
